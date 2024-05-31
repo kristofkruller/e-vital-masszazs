801979390446f2f222d8089cf2eb050aef80fdde
Kristof Kruller <kristof.kruller@gmail.com>
2024-05-31 20:55:36 +0200

    header.css, massages, class fix

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // landing menu
+  const menu = document.querySelector(".menuOpener");
+  // console.log(menu);
+  const menuToggle = document.querySelectorAll(".menuOpener, .nav-list, .nav-list-item, .also, .evital, .social, .opening, .adatkezelesi, .hero-text-2, .appointment-btn");
+
+  menu.addEventListener("click", function() {
+    menuToggle.forEach(e => {
+      if (e.classList.contains("show")) {
+        // landing menu content
+        if (e.classList.contains("menuOpener") && e.classList.contains("landing")) e.innerText = e.innerText.replace("x", "Menü");
+
+        // Disable transition for hiding
+        e.classList.add("no-transition");
+        e.classList.remove("show");
+
+        // Trigger reflow to apply no-transition class immediately
+        e.offsetHeight; // This line forces the browser to reflow
+
+        // Re-enable transition for showing
+        e.classList.remove("no-transition");
+      } else {
+        // landing menu content
+        if (e.classList.contains("menuOpener") && e.classList.contains("landing")) e.innerText = e.innerText.replace("Menü", "x");
+        e.classList.add("show");
+      }
+    });
+  });
+});
