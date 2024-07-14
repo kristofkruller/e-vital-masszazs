df2a9568f30eb4160f7515f9f8582bb521593c19
kristofkruller <kristof.kruller@gmail.com>
2024-07-14 20:15:34 +0200

    massages, prices respo, prices full rework, popup, bg img upgr, seo img, css upgr

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# e-vital-masszazs
-Évi Vitál Masszázs web 
+# Évi vital massage website
 
-##
-
-index.html
-// min vh logo-conti 50 
-- menu onclick min vh 100, menu hide 
-- menu X menu hide
-- display none -> opacity 0 -> 100ms opacity 1
-- basic transition 
-- content swap
-
-többi html 
-- hambi 
-- display none -> opacity 0 -> 100ms opacity 1
-- menu X menu hide
+/* MAX-WITH QUERIES
+For Mobile Devices – 320px — 480px
+iPads and Tablets – 481px — 768px
+Laptops and small screen – 769px — 1024px
+Large screens and Desktops – 1025px — 1200px
+TV and Extra Large Screens – 1201px and more */
---
+++ prices/prices.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //POPUP
+  const popup = document.querySelector(".popup")
+  const closePopUp = document.querySelector(".popup-close")
+  const popUpContent = document.querySelector(".popup-content")
+  let currentIcon = null
+
+  const showPopUp = (e) => {
+    const icon = e.target
+    const infoContent = icon.getAttribute("data-info")
+    if (infoContent) {
+      popUpContent.textContent = infoContent
+      const x = e.pageX
+      const y = e.pageY
+      popup.style.top = `${y + 5}px`
+      popup.style.left = `${x}px`
+      popup.style.display = "block"
+      currentIcon = icon
+    }
+  }
+
+  const hidePopUp = () => {
+    console.log(0)
+    popup.style.display = "none"
+    popUpContent.textContent = ""
+    currentIcon = null
+  }
+
+  // Attach event listeners to all .fa-info icons
+  const infoIcons = document.querySelectorAll(".fa-info")
+  infoIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      event.stopPropagation() // Prevent the event from reaching the document click handler
+      if (currentIcon === icon) {
+        hidePopUp()
+      } else {
+        showPopUp(event)
+      }
+    })
+  })
+
+  // Hide the popup when clicking x || outside
+  closePopUp.addEventListener("click", hidePopUp)
+  document.addEventListener("click", hidePopUp)
+})
